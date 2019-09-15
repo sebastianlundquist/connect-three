421de9efdf35157274abcb0b32f370e1ff81a93e
Sebastian Lundqvist <seblu507@student.liu.se>
2019-09-15 12:44:33 +0200

    Add dropin animation for tiles

--- app/src/main/java/com/sebastianlundquist/connectthree/MainActivity.java
+++ app/src/main/java/com/sebastianlundquist/connectthree/MainActivity.java
@@ -3,8 +3,17 @@ package com.sebastianlundquist.connectthree;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropin(View view) {
+        ImageView counter = (ImageView)view;
+        counter.setTranslationY(-1500);
+        counter.setImageResource(R.drawable.yellow);
+        counter.animate().translationYBy(1500).rotation(360).setDuration(500);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
