7d81a6e34b386012f067c22758249eeb0487f22f
Sebastian Lundqvist <seblu507@student.liu.se>
2019-09-15 13:37:41 +0200

    Add functionality to reset game and lock board when game is finished

--- app/src/main/java/com/sebastianlundquist/connectthree/MainActivity.java
+++ app/src/main/java/com/sebastianlundquist/connectthree/MainActivity.java
@@ -4,14 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // 0 is red, 1 is yellow
     int activePlayer = 0;
-
+    boolean gameActive = true;
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
     int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
@@ -19,25 +20,52 @@ public class MainActivity extends AppCompatActivity {
         ImageView counter = (ImageView)view;
 
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
-        gameState[tappedCounter] = activePlayer;
+        if (gameState[tappedCounter] == 2 && gameActive) {
+            gameState[tappedCounter] = activePlayer;
 
-        counter.setTranslationY(-1500);
-        if (activePlayer == 0) {
-            counter.setImageResource(R.drawable.red);
-            activePlayer = 1;
-        }
-        else {
-            counter.setImageResource(R.drawable.yellow);
-            activePlayer = 0;
-        }
-        counter.animate().translationYBy(1500).rotation(360).setDuration(500);
-
-        for (int[] winningPosition : winningPositions) {
-            if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
-                String winner = "";
-                winner = activePlayer == 1 ? "Red" : "Yellow";
-                Toast.makeText(this, winner + " has won!", Toast.LENGTH_SHORT).show();
+            counter.setTranslationY(-1500);
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 1;
             }
+            else {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1500).rotation(360).setDuration(500);
+
+            for (int[] winningPosition : winningPositions) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
+                    gameActive = false;
+                    String winner = "";
+                    winner = activePlayer == 1 ? "Red" : "Yellow";
+                    winner += " has won!";
+
+                    TextView winnerTextView = findViewById(R.id.winnerText);
+                    Button playAgain = findViewById(R.id.playAgain);
+                    winnerTextView.setText(winner);
+                    winnerTextView.setVisibility(View.VISIBLE);
+                    playAgain.setVisibility(View.VISIBLE);
+                }
+            }
+        }
+    }
+
+    public void resetGame(View view) {
+        TextView winnerTextView = findViewById(R.id.winnerText);
+        Button playAgain = findViewById(R.id.playAgain);
+        winnerTextView.setVisibility(View.INVISIBLE);
+        playAgain.setVisibility(View.INVISIBLE);
+        androidx.gridlayout.widget.GridLayout gridLayout = findViewById(R.id.gameBoard);
+
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            ImageView counter = (ImageView)gridLayout.getChildAt(i);
+            counter.setImageDrawable(null);
+        }
+        activePlayer = 0;
+        gameActive = true;
+        for (int i = 0; i < gameState.length; i++) {
+            gameState[i] = 2;
         }
     }
 
